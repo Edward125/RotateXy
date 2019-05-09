6ed2b6e798bfc098cd53c06bb5362497a68a8983
Edward <edward_song@yeah.net>
2019-05-09 17:28:47 +0800

    cos error

--- RotateXy/Form1.Designer.cs
+++ RotateXy/Form1.Designer.cs
@@ -28,20 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(239, 164);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(60, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(324, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(60, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(239, 216);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(60, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(324, 216);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(60, 21);
+            this.textBox4.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(508, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "y";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(434, 112);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 21);
+            this.textBox5.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(726, 497);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- RotateXy/Form1.cs
+++ RotateXy/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+
 namespace RotateXy
 {
     public partial class Form1 : Form
@@ -13,6 +14,67 @@ namespace RotateXy
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_angle"></param>
+        private double  Angel2Pi(int angle)
+        {
+           
+            double _angle = (double ) angle;
+
+
+
+            return (angle / 180.0) * Math.PI;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (string.IsNullOrEmpty (textBox1.Text.Trim ()))
+                return;
+            if (string.IsNullOrEmpty (textBox2.Text.Trim ()))
+                return;
+            if (string.IsNullOrEmpty (textBox5.Text.Trim ()))
+                return;
+
+            double radx = Angel2Pi (Convert.ToInt16 ( textBox5.Text.Trim()));
+
+
+
+            //textBox3.Text = Math.Sin(Math.PI / 6).ToString();
+
+            //MessageBox.Show(Math.Sin ( radx).ToString ());
+
+            //return;
+
+            MessageBox.Show(Math.Cos(Math.PI/2).ToString());
+
+
+            double x1 = Convert.ToDouble (textBox1.Text.Trim()) * Math.Cos(radx);
+            double x2 = Convert.ToDouble(textBox2.Text.Trim()) * Math.Sin(radx);
+            double y1 = Convert.ToDouble(textBox2.Text.Trim()) * Math.Cos(radx);
+            double y2 = Convert.ToDouble(textBox1.Text.Trim()) * Math.Sin(radx);
+
+
+            textBox3.Text = (x1-x2 ).ToString ();
+            textBox4.Text = (y1 + y2).ToString();
+           
         }
     }
 }
